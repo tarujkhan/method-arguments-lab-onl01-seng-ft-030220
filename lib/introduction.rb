1dbd774a57d38e660450e12f4ce31b55dfda5879
taroj khan <tarujkhan@yahoo.com>
2020-03-02 21:37:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,2 +1,3 @@
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program #{langua
+  puts "Hi, my name is #{name} and I am learning to program #{language}."
+end
